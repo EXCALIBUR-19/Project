50c381ce3fc6296654292959a26e01f3a07e08b0
abc <abc@example.com>
2021-07-08 16:50:15 +0530

    Project

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Who' },
+  { id: 12, name: 'Superman' },
+  { id: 13, name: 'Batman' },
+  { id: 14, name: 'Cat Women' },
+  { id: 15, name: 'Iron Man' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Spiderman' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
